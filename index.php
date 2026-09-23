7ead7312423e1b3e7023c87168bee0a5fc6add4c
JasmineCastroooo <Jasmine@div.tec.br>
2026-09-23 15:45:21 -0300

    feat: trata usuario nao encontrado

--- index.php
+++ index.php
@@ -2,14 +2,29 @@
 
 require_once __DIR__ . '/config/database.php';
 
-$email = 'teste@iforgot.local';
+$data = json_decode(
+    file_get_contents('php://input'),
+    true
+);
+
+$email = $data['email'] ?? '';
 
 $stmt = $pdo->prepare(
-    'SELECT id, email, password_hash, created_at FROM users WHERE email = ?'
+    'SELECT id, email, password_hash, created_at
+     FROM users
+     WHERE email = ?'
 );
 
 $stmt->execute([$email]);
 
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$user) {
+    echo json_encode([
+        'message' => 'Usuário não encontrado'
+    ]);
+
+    exit;
+}
 
 echo json_encode($user);
